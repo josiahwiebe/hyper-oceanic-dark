8404dc91729bb74a81ab57b51946a990a0a2761b
josiahwiebe <josiah@collectif.co>
2016-11-02 08:53:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+exports.decorateConfig = config => {
+  config.cursorColor = 'rgba(55, 59, 65, 0.5)';
+  config.foregroundColor = '#878986';
+  config.backgroundColor = '#112935';
+  config.borderColor = '#112935';
+  config.colors = [
+    '#2C2F33',
+    '#802426',
+    '#8F9547',
+    '#D7A24D',
+    '#405F74',
+    '#714E76',
+    '#4E7D78',
+    '#B7BBB7',
+    '#30373B',
+    '#803E3F',
+    '#919565',
+    '#D7B175',
+    '#4F6574',
+    '#725976',
+    '#5B7D79',
+    '#D7D5D2',
+    '#C5C8C6'
+  ];
+  return config;
+}
