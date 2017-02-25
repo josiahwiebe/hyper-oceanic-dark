b913e804dca9c5703e9eea3dd8538cc8136b85b7
josiahwiebe <josiah@collectif.co>
2017-02-25 11:02:42 -0600

    update code style

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-exports.decorateConfig = config => {
-  config.cursorColor = 'rgba(55, 59, 65, 0.5)';
-  config.foregroundColor = '#878986';
-  config.backgroundColor = '#1f3e4b';
-  config.borderColor = '#112935';
-  config.colors = [
-    '#2C2F33',
-    '#802426',
-    '#8F9547',
-    '#D7A24D',
-    '#405F74',
-    '#714E76',
-    '#4E7D78',
-    '#B7BBB7',
-    '#30373B',
-    '#803E3F',
-    '#919565',
-    '#D7B175',
-    '#4F6574',
-    '#725976',
-    '#5B7D79',
-    '#D7D5D2',
-    '#C5C8C6'
-  ];
-  return config;
+exports.decorateConfig = (config) => {
+  return Object.assign({}, config, {
+    cursorColor: 'rgba(55, 59, 65, 0.5)',
+    foregroundColor: '#878986',
+    backgroundColor: '#112935',
+    borderColor: '#112935',
+    colors: [
+      '#2C2F33',
+      '#802426',
+      '#8F9547',
+      '#D7A24D',
+      '#405F74',
+      '#714E76',
+      '#4E7D78',
+      '#B7BBB7',
+      '#30373B',
+      '#803E3F',
+      '#919565',
+      '#D7B175',
+      '#4F6574',
+      '#725976',
+      '#5B7D79',
+      '#D7D5D2',
+      '#C5C8C6'
+    ]
+  })
 }
